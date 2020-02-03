bab8d361f1d9ed4f868fe80b1b93a254304b9673
samuel-robinet <samuel.robinet@etu-umontpellier.fr>
2020-02-03 11:00:32 +0100

    R20

--- td2.sql
+++ td2.sql
@@ -32,3 +32,60 @@ join Campings Ca  on Ca.idCamping=B.idCamping
 where nomCamping='Les Flots Bleus' and dateDebut<='14/07/2017' and dateFin>='14/07/2017';
 
 R16
+select nomClient, prenomClient
+from Clients C
+where idClient in (
+	select idClient
+from Locations L
+join Bungalows B on L.idBungalow=B.idBungalow
+join Campings Ca  on Ca.idCamping=B.idCamping
+where nomCamping='Les Flots Bleus' and dateDebut<='31/07/2017' and dateFin>='01/07/2017');
+
+R17
+
+select nomClient, prenomClient
+from Clients C
+where idClient in ( select idClient
+from Locations L 
+join Bungalows B on L.idBungalow=B.idBungalow
+join Campings Ca  on Ca.idCamping=B.idCamping
+where villeClient=villeCamping);
+
+select nomClient, prenomClient, villeCamping, villeClient,idClient
+from Clients C
+join Locations L on C.idClient=L.idClient
+join Bungalows B on L.idBungalow=B.idBungalow
+join Campings Ca  on Ca.idCamping=B.idCamping
+where villeClient=villeCamping;
+
+Select nomClient, prenomClient
+from Client C
+Where villeClient in (Select villeCamping
+from Campings)
+
+R18
+select count(S.idservice)
+from Services S
+join Proposer P on P.idservice=S.idservice
+join Bungalows B on P.idBungalow=B.idBungalow
+where nomBungalow='Le Titanic'
+
+R19
+
+select max(salaireEmploye)
+from Employes E
+join Campings C on E.idCamping=C.idCamping
+where nomCamping='Les Flots Bleus'
+
+R20
+select count(Ca.idCamping)
+from Campings Ca
+join Bungalows B on Ca.idCamping=B.idCamping
+join Locations L on B.idBungalow=L.idBungalow
+join Clients C on L.idClient=C.idClient
+where nomClient='Zeblouse'
+
+select count(idLocation)
+join Locations 
+join Clients C on L.idClient=C.idClient
+where nomClient='Zeblouse'
