74549b75a066b85c642794458aaab3bfe07825b9
Samuel Robinet <samuel.robinet@etu-umontpellier.fr>
2020-02-03 09:50:59 +0100

    R15

--- td2.sql
+++ td2.sql
@@ -22,3 +22,11 @@ select E.nomEmploye,E.prenomEmploye
 from Employes E
 join Employes E2 on E2.idEmploye=E.idEmployeChef #et pas E.idEmploye=E2.idEmployeChe
 where E2.nomEmploye='Alizan' and E2.prenomEmploye='Gaspard';
+
+R15
+select nomClient, prenomClient, dateDebut, dateFin
+from Clients C
+join Locations L on C.idClient=L.idClient
+join Bungalows B on L.idBungalow=B.idBungalow
+join Campings Ca  on Ca.idCamping=B.idCamping
+where nomCamping='Les Flots Bleus' and dateDebut<='14/07/2017' and dateFin>='14/07/2017';
