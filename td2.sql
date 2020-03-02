e68bad140b0b09eb0862061a94143253c0f9fdc1
samuel-robinet <samuel.robinet@etu-umontpellier.fr>
2020-03-02 12:01:37 +0100

    kk

--- td2.sql
+++ td2.sql
@@ -124,9 +124,202 @@ Select idEmployeChef
 from Employes)
 
 R25
-Select nomBungalow
-from Bungalows B
+Select Distinct nomBungalow
+from Bungalows 
+where idBungalow not in(select idBungalow
+						from Proposer P 
+							join Services S on P.idservice=S.idservice
+						where nomService='Climatisation') 
+
+and idBungalow not in (select idBungalow
+						from Proposer P 
+							join Services S on P.idservice=S.idservice
+						where nomService='Service TV'))
+					  
+
+
 join Proposer P on B.idBungalow=P.idBungalow
 join Services S on P.idservice=S.idservice
-where idservice not in(select idService 
-	='Climatisation'
+
+
+
+
+
+
+
+
+
+Td3.
+
+R30
+
+select nomBungalow
+from Bungalows B
+where not exists (select *
+					from Locations L 
+					where B.idBungalow=L.idBungalow)
+
+
+R31
+
+select nomCamping
+from Campings C
+where not exists (select *
+					from Employes E
+					where C.idCamping=E.idCamping)
+
+R32
+
+
+select nomService
+from Services S
+where categorieService='Loisir' or not exists (select*
+												from Proposer P
+												join Bungalows B on P.idBungalow=B.idBungalow
+												join Campings C on B.idCamping=C.idCamping
+												where S.idservice=P.idservice and nomCamping='The White Majestic')
+
+
+
+/*
+select*
+from Services S												
+join Proposer P on S.IdService=P.idService
+												join Bungalows B on P.idBungalow=B.idBungalow
+												join Campings C on B.idCamping=C.idCamping
+												where nomCamping='The White Majestic';*/
+
+R33 
+
+Marche mais pas comme il faut
+
+select nomClient 
+from Clients C
+join Locations L on C.idClient=L.idClient
+where not exists (select *
+				from Locations L
+				join Bungalows B on L.idBungalow=B.idBungalow
+				where superficieBungalow<58 and C.idClient=L.idClient)
+
+
+/*
+select *
+from Clients C
+join Locations L on C.idClient=L.idClient
+				join Bungalows B on L.idBungalow=B.idBungalow
+				where superficieBungalow<58 */
+R34
+/* Marche pas */
+select nomClient
+from Clients C
+where villeClient='Montpellier'
+and not exists (select *
+				from Bungalows B
+				join Proposer P on B.idBungalow=P.idBungalow
+				join Locations L on B.idBungalow=L.idBungalow
+				where C.idClient=L.idBungalow 
+				and not exists( select *
+							from Services S
+							where P.idservice=S.idService))
+
+R40
+select distinct c.idClient, c.nomClient, c.prenomClient
+from Clients C
+join Locations L on C.idClient=L.idClient
+join Bungalows B on L.idBungalow=B.idBungalow
+join Campings Ca on Ca.idCamping=B.idCamping
+where C.villeClient=Ca.villeCamping
+
+R41
+select nomEmploye
+from Employes E
+where not exists (select*
+							from Campings C
+							where E.idCamping=C.idCamping)
+
+TD4
+
+R5A
+select categorieService, count(idservice)
+from Services 
+group by categorieService
+
+R5B
+select villeClient
+from Clients
+group by villeClient
+having count(villeClient)>=3
+
+R50
+select nomClient, prenomClient, count(idLocation) as "Nb Location"
+from Clients C
+join Locations L on C.idClient=L.idClient
+group by C.idClient, nomClient, prenomClient
+order BY  count(idLocation) DESC
+
+R51
+select nomCamping, AVG(salaireEmploye)
+from Campings C
+join Employes E on C.idCamping=E.idCamping
+group by C.idCamping, nomCamping
+
+R52
+select nomCamping
+from Campings C
+join Employes E on C.idCamping=E.idCamping
+group by C.idCamping, nomCamping
+having count(idEmploye)>3
+
+R53
+select nomCamping
+from Campings C
+join Employes E on C.idCamping=E.idCamping
+group by C.idCamping, nomCamping
+having AVG(SalaireEmploye)>1400
+
+R54
+select nomCamping
+from Campings C
+Join Bungalows B on C.idCamping=B.idCamping
+where superficieBungalow<65
+group by C.idCamping, nomCamping
+order by count(superficieBungalow)
+
+R55
+/* Aussi en comptant et not exist */
+select nomCamping
+from Campings C
+join Employes E on C.idCamping=E.idCamping
+group by C.idCamping, nomCamping
+having min(salaireEmploye)>=1000
+
+
+
+R56
+Select nomBungalow
+from Bungalows B 
+join Proposer P on B.idBungalow=P.idBungalow
+group by B.idBungalow, nomBungalow
+having count(P.idservice) = (select count(idservice) 
+							from Bungalows B 
+							join Proposer P on B.idBungalow=P.idBungalow
+							where nomBungalow='Le Royal')
+
+R57
+/* Left permet d'avoir les 0*/
+select nomBungalow, count(idservice) as "Nb Services"
+from Bungalows B 
+Left join Proposer P on B.idBungalow=P.idBungalow
+group by B.idBungalow, nomBungalow
+order by count(idservice) DESC
+
+
+R58
+/*Marche pas*/
+select nomBungalow, nomCamping
+from Bungalows B
+join Campings C on  b.idCamping=C.idCamping
+join Locations L on B.idBungalow=L.idBungalow
+where dateDebut<='31/06/2017' and dateFin>='01/06/2017'
+group by B.idBungalow, nomBungalow, nomCamping
+having count(idLocation)>2
